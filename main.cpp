391b89711a81706fdec61102dd8e29a9b860c49c
Impally <Rodger.Friesen@gmail.com>
2015-05-18 23:47:40 -0400

    Fixing updates.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include "GL\glew.h"
-#include "GL\freeglut.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include "Core\Shader_Loader.h"
+
+using namespace Core;
+
+GLuint program;
+
+void renderScene(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 0.0,1.0, 1.0); //just red
+
+	//use the created program
+	glUseProgram(program);
+
+	//draw 3 vertices as triangles.
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 2, 3);
+	glDrawArrays(GL_TRIANGLES, 4, 3);
+	glDrawArrays(GL_TRIANGLES, 7, 3);
+	glDrawArrays(GL_TRIANGLES, 10, 3);
+	glDrawArrays(GL_TRIANGLES, 13, 3);
+	glDrawArrays(GL_TRIANGLES, 16, 3);
+	glDrawArrays(GL_TRIANGLES, 19, 3);
+	glutSwapBuffers();
+}
+
+void Init()
+{
+	glEnable(GL_DEPTH_TEST);
+
+	//load and compile shaders
+	Core::Shader_Loader shaderLoader;
+	program = shaderLoader.CreateProgram("Shaders\\Vertex_Shader.glsl",
+		"Shaders\\Fragment_Shader.glsl");
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
 
 int main(int argc, char **argv)
 {
@@ -8,9 +46,14 @@ int main(int argc, char **argv)
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(500, 500);
 	glutInitWindowSize(800, 600);
-	glutCreateWindow("OpenGL First Window");
+	glutCreateWindow("First Triangle");
 
+	glewInit();
+	Init();
+
+	//register callbacks
+	glutDisplayFunc(renderScene);
 	glutMainLoop();
-
+	glDeleteProgram(program);
 	return 0;
 }
